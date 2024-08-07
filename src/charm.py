55882029fc9dd99f74ccae77af5d923f4ea665d8
IbraAoad <Ibrahim.Awwad@canonical.com>
2024-08-07 15:41:08 +0300

    draft waypoint

--- src/charm.py
+++ src/charm.py
@@ -6,10 +6,30 @@
 """Istio Beacon Charm."""
 
 import logging
+import time
 
 import ops
+from lightkube_extensions.batch import KubernetesResourceManager, create_charm_default_labels
+from lightkube.generic_resource import create_namespaced_resource
+from lightkube.core.client import Client
+from models import Metadata, Listener, IstioWaypointSpec, IstioWaypointResource, AllowedRoutes
+from lightkube.models.meta_v1 import ObjectMeta
+from ops.model import ActiveStatus, MaintenanceStatus, BlockedStatus
+from ops import Relation
+from lightkube.resources.apps_v1 import Deployment
+from lightkube.core.exceptions import ApiError
+from lightkube.resources.core_v1 import Namespace
 
 logger = logging.getLogger(__name__)
+
+RESOURCE_TYPES = {
+    "Gateway": create_namespaced_resource(
+        "gateway.networking.k8s.io", "v1", "Gateway", "gateways"
+    ),
+}
+
+WAYPOINT_RESOURCE_TYPES = {RESOURCE_TYPES["Gateway"]}
+WAYPOINT_LABEL = "istio-waypoint"
 
 
 class IstioBeaconCharm(ops.CharmBase):
@@ -17,28 +37,151 @@ class IstioBeaconCharm(ops.CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.start, self.on_start)
+
+        self._lightkube_field_manager: str = self.app.name
+        self._lightkube_client = None
+
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.remove, self._on_remove)
         self.framework.observe(self.on["service-mesh"].relation_changed, self.on_mesh_changed)
+        self.framework.observe(self.on["service-mesh"].relation_broken, self.on_mesh_broken)
 
-    def on_start(self, _event):
-        """Event handler for start."""
-        self.unit.status = ops.ActiveStatus()
+    def _on_config_changed(self, _):
+        """Event handler for config changed."""
+        self._sync_all_resources()
 
-    def on_mesh_changed(self, _event):
+    def on_mesh_changed(self, _):
         """Event handler for service-mesh relation_changed."""
-        self.update_mesh()
+        self._sync_all_resources()
 
-    def update_mesh(self):
-        """Update the service mesh.
+    def on_mesh_broken(self, _):
+        """Event handler for service-mesh relation_broken."""
+        self._sync_all_resources()
 
-        Reads all relations and any config to generate a kubernetes manifest. Then applies the
-        manifest.
-        """
-        for relation in self.model.relations["service-mesh"]:
-            logger.error(relation.data[relation.app])
-            # Update the mesh
-        self.unit.status = ops.ActiveStatus()
+    def _on_remove(self, _):
+        """Event handler for remove."""
+        self._remove_labels()
+        krm = self._get_waypoint_resource_manager()
+        krm.delete()
 
+    @property
+    def lightkube_client(self):
+        """Returns a lightkube client configured for this charm."""
+        if self._lightkube_client is None:
+            self._lightkube_client = Client(
+                namespace=self.model.name, field_manager=self._lightkube_field_manager
+            )
+        return self._lightkube_client
+
+    def _get_waypoint_resource_manager(self):
+        return KubernetesResourceManager(
+            labels=create_charm_default_labels(
+                self.app.name, self.model.name, scope=WAYPOINT_LABEL
+            ),
+            resource_types=WAYPOINT_RESOURCE_TYPES,  # pyright: ignore
+            lightkube_client=self.lightkube_client,
+            logger=logger,
+        )
+
+    def _is_deployment_ready(self) -> bool:
+        """Check if the deployment is ready after 10 attempts."""
+        timeout = int(self.config["ready-timeout"])
+        check_interval = 10
+        attempts = timeout // check_interval
+
+        for _ in range(attempts):
+            try:
+                deployment = self.lightkube_client.get(
+                    Deployment,
+                    name=f"{self.app.name}-{self.model.name}-waypoint",
+                    namespace=self.model.name,
+                )
+                if (
+                    deployment.status
+                    and deployment.status.readyReplicas == deployment.status.replicas
+                ):
+                    return True
+            except ApiError as e:
+                logger.error(f"Error checking waypoint deployment status: {e}")
+
+            time.sleep(check_interval)
+
+        return False
+
+    def _is_ready(self) -> bool:
+
+        if not self._is_deployment_ready():
+            return False
+        return True
+
+    def _sync_all_resources(self):
+        self.unit.status = MaintenanceStatus("Validating waypoint readiness")
+        self._sync_waypoint_resources()
+        if self._is_ready():
+            self.unit.status = BlockedStatus(
+                f"Waypoint's k8s deployment not ready, is istio properly installed?"
+            )
+            return
+        self.unit.status = ActiveStatus()
+
+    def _construct_waypoint(self):
+        gateway = IstioWaypointResource(
+            metadata=Metadata(
+                name=f"{self.app.name}-{self.model.name}-waypoint",
+                namespace=self.model.name,
+                labels={"istio.io/waypoint-for": "service"},
+            ),
+            spec=IstioWaypointSpec(
+                gatewayClassName=f"istio-waypoint",
+                listeners=[
+                    Listener(
+                        name="mesh",
+                        port=15008,
+                        protocol="HBONE",
+                        allowedRoutes=AllowedRoutes(namespaces={"from": "All"}),
+                    )
+                ],
+            ),
+        )
+        gateway_resource = RESOURCE_TYPES["Gateway"]
+        return gateway_resource(
+            metadata=ObjectMeta.from_dict(gateway.metadata.dict()),
+            spec=gateway.spec.dict(),
+        )
+
+    def _sync_waypoint_resources(self):
+        resources_list = []
+        if not self.unit.is_leader():
+            raise RuntimeError("Waypoint can only be provided on the leader unit.")
+        krm = self._get_waypoint_resource_manager()
+        resource_to_append = self._construct_waypoint()
+        resources_list.append(resource_to_append)
+        krm.reconcile(resources_list)
+        namespace = self.lightkube_client.get(Namespace, self.model.name)
+
+        if self.config["model-on-mesh"]:
+            labels = {
+                "istio.io/use-waypoint": f"{self.app.name}-{self.model.name}-waypoint",
+                "istio.io/dataplane-mode": "ambient",
+            }
+
+            namespace.metadata.labels.update(labels)
+            self.lightkube_client.patch(Namespace, self.model.name, namespace)
+        else:
+            self._remove_labels()
+
+    def _remove_labels(self):
+        namespace = self.lightkube_client.get(Namespace, self.model.name)
+        """Remove specific labels from a namespace if they exist."""
+        labels_to_remove = {"istio.io/use-waypoint": None, "istio.io/dataplane-mode": None}
+
+        # Check if the labels exist and remove them by setting to None
+        if namespace.metadata.labels and (
+            "istio.io/use-waypoint" in namespace.metadata.labels
+            or "istio.io/dataplane-mode" in namespace.metadata.labels
+        ):
+            namespace.metadata.labels.update(labels_to_remove)
+            self.lightkube_client.patch(Namespace, self.model.name, namespace)
 
 if __name__ == "__main__":
     ops.main(IstioBeaconCharm)  # type: ignore
