d319cdf3e0c7ceb186c51922fd5c2dd6924af44f
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2024-09-19 14:20:39 +0000

    scenario 7

--- tests/scenario/test_charm.py
+++ tests/scenario/test_charm.py
@@ -9,5 +9,5 @@ from charm import IstioBeaconCharm
 # TODO: introduce mocks needed for testing model-on-mesh
 def test_relation_changed_status():
     ctx = scenario.Context(IstioBeaconCharm)
-    out = ctx.run("start", scenario.State())
+    out = ctx.run(ctx.on.start(), scenario.State())
     assert out.unit_status.name == "unknown"
